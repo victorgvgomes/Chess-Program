edbb2f219d7c26607d427ffbf342c4d99866fec2
Victor Gomes <victorgvgomes@gmail.com>
2021-05-26 21:52:44 -0300

    Moving pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import Chess.ChessPiece;
+import Chess.ChessPosition;
 import Chess.Color;
 
 public class UI {
@@ -23,6 +27,24 @@ public class UI {
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	
+	
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try{
+		String s= sc.nextLine();
+		char column= s.charAt(0);
+		int row= Integer.parseInt(s.substring(1));
+		return new ChessPosition(column, row);
+		}catch(RuntimeException e) {
+			throw new InputMismatchException ("Error reading ChessPosition. Valid values are from a1 to h8");
+			
+		}
+		
+		
+		
+		
+	}
 
 	
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -45,13 +67,6 @@ public class UI {
 	
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-			System.out.print("-");
-		}
-		else {
-			System.out.print(piece);
-		}
-		System.out.print(" ");
-    	if (piece == null) {
             System.out.print("-");
         }
         else {
