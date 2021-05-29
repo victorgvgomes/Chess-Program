108d161c07e5de6580108dd1636c2b3ab3c190e7
Victor Gomes <victorgvgomes@gmail.com>
2021-05-28 22:56:31 -0300

    Knight

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import Chess.piece.Bishop;
 import Chess.piece.King;
+import Chess.piece.Knight;
 import Chess.piece.Pawn;
 import Chess.piece.Rook;
 import boardgame.Board;
@@ -196,8 +197,10 @@ public class ChessMatch {
 
 	private void initialSetup() {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
-
+	
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
@@ -227,6 +230,8 @@ public class ChessMatch {
 		placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
+		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 	}
 
 	public Color getCurrentPlayer() {
